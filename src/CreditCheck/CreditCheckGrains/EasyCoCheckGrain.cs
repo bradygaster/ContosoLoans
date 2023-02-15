5f71dc0c92686ea674c99e7b523055a05a47f055
Setup <setup@example.com>
2023-02-15 13:35:55 -0800

    made easy slow

--- src/CreditCheck/CreditCheckGrains/EasyCoCheckGrain.cs
+++ src/CreditCheck/CreditCheckGrains/EasyCoCheckGrain.cs
@@ -9,6 +9,10 @@ public class EasyCoCheckGrain : CreditCheckGrainBase {
     public override async Task<CreditCheck> Validate(LoanApplication app) {
         if (!_state.RecordExists) {
             _logger.LogInformation($"Credit check for {app.ApplicationId} with {Constants.EASY_CO} started");
+
+            // simulate a longer running check
+            await Task.Delay(Random.Shared.Next(1000, 3000));
+            
             _state.State = new CreditCheck {
                 Agency = Constants.EASY_CO,
                 ApplicationId = this.GetPrimaryKey(),
