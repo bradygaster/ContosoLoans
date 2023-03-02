24e6c0d3ab1cd1bbd88ef4ac4aebb7b382883bfc
Setup <setup@example.com>
2023-03-01 17:27:06 -0800

    added default dashboard for viewing orleans metrics

--- execute-http-call-loop.ps1
+++ execute-http-call-loop.ps1
@@ -1,4 +1,4 @@
-for ($i = 0; $i -lt 5; $i++) {
+for ($i = 0; $i -lt 10; $i++) {
     $postParams = @{customerId=((New-Guid).ToString());loanAmount=(Get-Random -Minimum 1000 -Maximum 20000)}
     Invoke-WebRequest -Uri http://localhost:5002/loans -Method POST -Body ($postParams|ConvertTo-Json) -ContentType "application/json"
 }
